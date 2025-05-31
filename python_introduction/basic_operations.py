14c0b87d2f3e7a114d7137d2e5724c5d4d4ed6bf
LilSunBurst <3ul0rdoz@duck.com>
2025-05-31 20:31:08 +0300

    Practicing badic arithmetic calculations with python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10 
+number2 = 5
+sum = number1 + number2
+print ("Addition of 10 and 5 is", sum)
+difference = number1 - number2
+print ("Subtraction of 10 and 5 is", difference)
+product = number1 * number2
+print ("Multiplication of 10 and 5 is", product)
