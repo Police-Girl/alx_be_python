cdeb7dddc912d78d208d3d8959e63b6812bfa49a
LilSunBurst <3ul0rdoz@duck.com>
2025-06-06 19:54:19 +0300

    updates

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -6,6 +6,6 @@ elif weather == "rainy":
 elif weather == "cold":
 	print("Make sure to wear a warm coat and a scarf.")
 else:{
-        print("Sorry I don't have a recommendation for this weather.")
+        print("Sorry, I don't have a recommendation for this weather.")
 }
 
