56640215349dfde64c59e41f63fcd126b15d9267
LilSunBurst <3ul0rdoz@duck.com>
2025-06-07 19:22:33 +0300

    updates ffs

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 tableNum = int(input("Enter a number to see its multiplication table:"))
-tableSize = range(1,11)
+#tableSize = range(1,11)
 #results = tableNum * tableSize
-for x in tableSize:
+for x in range (1,11):
 	print(f"{tableNum} * {x} = {tableNum * x}")
 
