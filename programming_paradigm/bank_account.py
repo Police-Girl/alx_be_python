552510518ac4c5752642a4c540cac870bcb79205
LilSunBurst <3ul0rdoz@duck.com>
2025-07-06 16:08:04 +0300

    Bank Account sim

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+	def __init__(self,initial_balance = 0): #idk man, the initial ammount? # with the BA's we just need one attribute??? damn bro.
+	#	self.balance = balance
+	#	self.deposit = deposit
+	#	self.withdraw = withdraw
+	#	self.display = display
+		self.account_balance = initial_balance
+	def deposit(self,amount):	# you did not define this correctly it seemsdef deposit(ammount):
+		self.account_balance += amount
+		#return f"Deposited{deposit}"
+	def withdraw(self, amount):
+		if amount <= self.account_balance:
+			self.account_balance -= amount
+			return True
+		else:
+			return False
+	def display_balance(self):
+		print (f"current balance: ${self.account_balance}")
+			#return f"Current baklance: ${display}"
+
