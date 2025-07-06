ce33c7d767fea481b50199893acb141d1798d280
LilSunBurst <3ul0rdoz@duck.com>
2025-07-07 01:07:21 +0300

    Unit test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,22 +6,22 @@ class TestSimpleCalculator(unittest.TestCase):
 		"""Sets up a new SimpleCalculatoe instance for each test."""
 		self.calc = SimpleCalculator()
 
-	def test_addition(self):
+	def test_add(self):
 		""" Return the addition of a and b"""
 		#result = add(4, 5)
 		self.assertEqual(self.calc.add (10 , 10), 20)
 
-	def test_subtraction(self):
+	def test_subtract(self):
 		"""Return the difference between a and b"""
 		#result = subtract(10, 4)
 		self.assertEqual(self.calc.subtract(10,4),6)
 
-	def test_multiplication(self):
+	def test_multiply(self):
 		"""Return the product of an and b"""
 		#result = multiply(2 , 7)
 		self.assertEqual(self.calc.multiply (2,7), 14)
 
-	def test_division(self):
+	def test_divide(self):
 		"""Return the modulus of a and b"""
 		#result = divide (4 , 2)
 		self.assertEqual(self.calc.divide(20,2),10)
