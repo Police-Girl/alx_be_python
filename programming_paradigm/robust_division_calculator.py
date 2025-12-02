7074c52f1dab1f178124b4082e6d327fd8a7e19d
Solana <147794376+Police-Girl@users.noreply.github.com>
2025-12-02 14:26:15 +0300

    Refactor safe_divide to include result message

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,12 +1,12 @@
-def safe_divide(numerator,denominator):
-	try:
-		num = float(numerator)
-		den = float(denominator)
-	except ValueError:
-		return f"Error: Please enter numeric values only."
-	try:
-		result = num/den
-		return result
-	except ZeroDivisionError:
-		return f"Error: Cannot divide by zero."
-	
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+		try:
+            result = num / den
+            return f"The result of the division is {result}"
+        except ZeroDivisionError:
+            return "Error: Cannot divide by zero."
+
+    except ValueError:
+        return "Error: Please enter numeric values only."
